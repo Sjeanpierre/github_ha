c4428b14051cd9c57f3c562209816f391dc349b3
stevenson.jean-pierre@sage.com <stevenson.jean-pierre@sage.com>
2013-05-20 17:08:38 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubHa::Application.config.secret_token = '62f6be2393460be5e92821230d5cae4bd53a6411506442314d46f8a13d0b20285f1a82278d99d50cc96a091c8c0c189e6db8d160736278d57722cb33e68a7912'
