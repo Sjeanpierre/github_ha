1f7d2f2ce1ea8fc0c355d1ce04f0dc020a799e2c
stevenson.jean-pierre@sage.com <stevenson.jean-pierre@sage.com>
2013-05-21 15:39:23 -0400

    added bootstap thin and simple form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+
 //= require jquery
-//= require jquery_ujs
+//= require jquery.dataTables
+//= require github
+//= require_tree ../../../vendor/assets/javascripts
 //= require_tree .
